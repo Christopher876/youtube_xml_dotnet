cf76937b07db28fcc72584b11f8d96aefa3471cb
Christopher Williams <christopher876@outlook.com>
2020-02-27 16:16:54 -0500

    Add video thumbnails and upload date

--- Parser.cs
+++ Parser.cs
@@ -7,7 +7,6 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 class Parser{
-    public List<Video> videos;
     private JObject jsonDocument;
 
     public Parser(XDocument doc){
@@ -22,15 +21,28 @@ class Parser{
         return jsonDocument["feed"]["entry"].Select(x => x["link"]["@href"]).ToArray();
     }
 
+    private JToken[] ParseUploadDate(){
+        return jsonDocument["feed"]["entry"].Select(x => x["published"]).ToArray();
+    }
+
+    private JToken[] ParseThumbnail(){
+        return jsonDocument["feed"]["entry"].Select(x => x["media:group"]["media:thumbnail"]["@url"]).ToArray();
+    }
+
     public Video[] ParseVideos()
     {
+        //Parse all the video details
         var videoTitles = ParseTitles(); //Get all of the video titles that the XML contains
         var videoIds = ParseIds();
+        var videoUploadDates = ParseUploadDate();
+        var videoThumbnails = ParseThumbnail();
+
+        //Create and populate the video array
         Video[] videos = new Video[videoTitles.Length];
         for (int i = 0; i < videos.Length; i++)
         {
-            videos[i] = new Video(name: videoTitles[i].ToString(),id:videoIds[i].ToString(),0,new DateTime());
+            videos[i] = new Video(name: videoTitles[i].ToString(),id:videoIds[i].ToString(),views:0,uploadDate:DateTime.Parse(videoUploadDates[i].ToString()),thumbnail:videoThumbnails[i].ToString());
         }
-        return null;
+        return videos;
     }
 }
