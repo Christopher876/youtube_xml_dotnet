22cea7439f7fa9ed642fc39f5af3e36ae0ecbb0d
Christopher Williams <christopherwilliams@Christophers-MacBook-Pro.local>
2020-02-27 13:20:26 -0500

    Add basic youtube channel properties

---
+++ Channel.cs
@@ -0,0 +1,25 @@
+using System.Xml.Linq;
+using System.Net.Http;
+using System;
+
+class YoutubeChannel{
+    private readonly string baseUrl = "https://www.youtube.com/feeds/videos.xml?channel_id="; 
+    string author;
+    string id;
+    public YoutubeChannel(string author, string url){
+        this.author = author;
+        this.id = url;
+    }
+
+    public XDocument getChannelFeed(){
+        string url = baseUrl+id;
+        string result = "";
+        using (var client = new HttpClient())
+        {
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            result = response.Content.ReadAsStringAsync().Result;
+        }
+        return XDocument.Parse(result);
+    }
+}
