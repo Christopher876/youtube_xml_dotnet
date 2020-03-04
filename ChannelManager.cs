5e0418924eff54f2d6b63c3bf94050c2e8f73757
Christopher Williams <christopher876@outlook.com>
2020-03-03 19:32:41 -0500

    Able to Generate a channel list json from youtube ids - Able to parse a txt file with a list of youtube channel ids and create a json for the channels - Generating the same json using channel names instead of ids is broken for now, spaced usernames are not being found every time - Refactor functions from Channel into the manager to handle getting feeds and ids - Cleanup some code to utilize DRY

--- ChannelManager.cs
+++ ChannelManager.cs
@@ -5,12 +5,143 @@ using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.IO;
+using System.Net.Http;
+using HtmlAgilityPack;
+using System.Threading;
+using System.Xml.Linq;
 
 class ChannelManager{
     List<YoutubeChannel> channels;
     public ChannelManager(IEnumerable<YoutubeChannel> channels = null){
         if(channels != null)
             this.channels = channels.ToList();
+    }
+
+    /// <summary>
+    /// Generate an inital database and channel list json using a txt file
+    /// </summary>
+    public void GenerateInitialManager(){
+        List<YoutubeChannel> _channels = new List<YoutubeChannel>();
+
+        if(File.Exists(@"channel-list.txt")){
+            //Get all the ids
+            List<string> s = File.ReadAllLines(@"channel-list.txt").ToList();
+
+            //Get the author name
+            foreach(var userId in s){
+                _channels.Add(new YoutubeChannel{
+                    author = getChannelAuthor(userId),
+                    id = userId,
+                });
+                Thread.Sleep(500); //Probably good to sleep in between requests
+            }
+
+            //Let's save all of the gathered channels to the json
+            //If it exists, start with this json
+            JObject json;
+            if(File.Exists(@"youtube-channels.json")){
+                //If file is empty, create the file hierarchy
+                if(new FileInfo(@"youtube-channels.json").Length == 0){
+                    json = new JObject(new JProperty("channels"));
+                    File.WriteAllText(@"youtube-channels.json",JsonConvert.SerializeObject(json));
+                }
+
+                json = JObject.Parse(File.ReadAllText(@"youtube-channels.json"));
+                JArray channelsArray = (JArray)json["channels"];
+                //After checking if it exists, lets add all the channels with their ids to the json
+                foreach(var channel in _channels){
+                    channelsArray.Add(new JObject{
+                        new JProperty("author",channel.author),
+                        new JProperty("channel-id",channel.id)
+                    });
+                }
+
+                //Set the class channels variable to the all of the new channels that were found and save the file
+                SetChannels(json);
+                SaveChannelList();
+            }
+            //TODO handle the creation of the file if the file does not exist
+            else{
+
+            }
+        }
+        else{
+            return;
+        }
+    }
+
+    //TODO Search for username and actually find it everytime if there are spaces
+    //Spaced user names are broken
+    public void GenerateInitialManager(string filename="", bool isFile = false, string channelName = ""){
+        List<YoutubeChannel> _channels;
+        JObject json;
+        if(isFile){
+            //Get some sort of list of channels that we want to use to generate youtube-channels.json
+            if(File.Exists(filename)){
+                //Load all of the channel names
+                List<string> s = File.ReadAllLines(filename).ToList();
+                _channels = new List<YoutubeChannel>();
+                foreach(var c in s){
+                    _channels.Append(new YoutubeChannel{
+                        author = c,
+                        id = getChannelId(c)
+                    });
+                }
+            } 
+            else{
+                return;
+            }
+
+            //Get all of the channels and add them to the json
+
+            //If it exists, start with this json
+            if(File.Exists(@"youtube-channels.json")){
+                //If file is empty, create the file hierarchy
+                if(new FileInfo(@"youtube-channels.json").Length == 0){
+                    json = new JObject(new JProperty("channels"));
+                }
+
+                //After checking if it exists, lets add all the channels with their ids to the json
+            }
+        }
+        else{
+            Console.WriteLine("Added Something");
+        }
+    }
+
+    private XDocument getChannelFeed(string id){
+        string url = Utils.channelBaseUrl+id;
+        string result = "";
+        using (var client = new HttpClient())
+        {
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            response.EnsureSuccessStatusCode();
+            result = response.Content.ReadAsStringAsync().Result;
+        }
+        return XDocument.Parse(result);
+    }
+
+    private string getChannelId(string author){
+        string result;
+
+        //We get the channel html
+        Thread.Sleep(500); //Sleep for half a second
+        using(var client = new HttpClient()){
+            HttpResponseMessage response = client.GetAsync("https://www.youtube.com/user/"+author).Result;
+            response.EnsureSuccessStatusCode();
+            result = response.Content.ReadAsStringAsync().Result;
+        }
+
+        //Process the html to get the channel id
+        var htmlDoc = new HtmlDocument();
+        htmlDoc.LoadHtml(result);
+        string channelId = htmlDoc.DocumentNode.SelectSingleNode("/html/head/link[5]").Attributes[1].Value;
+        return channelId.Replace("https://www.youtube.com/channel/","");
+    }
+
+    private string getChannelAuthor(string id){
+        var name = new Parser(getChannelFeed(id)).ParseChannelName();
+        return name.ToString();
     }
 
     /// <summary>
@@ -63,14 +194,22 @@ class ChannelManager{
         File.WriteAllText(@"youtube-channels.json",JsonConvert.SerializeObject(json));  
     }
 
+    private void SetChannels(JObject json){
+        channels = new List<YoutubeChannel>();
+        var _channels = json["channels"];
+        foreach(var channel in _channels){
+            this.channels.Add(new YoutubeChannel{
+                author = channel["author"].ToString(),
+                id = channel["channel-id"].ToString()
+            });
+        }
+    }
+
     /// <summary>
     /// Load the youtube channels from local storage
     /// </summary>
     public void LoadChannelList(){
         JObject json = JObject.Parse(File.ReadAllText(@"youtube-channels.json"));
-        var channels = json["channels"];
-        foreach(var channel in channels){
-            this.channels.Append(new YoutubeChannel(channel["author"].ToString(),channel["channel-id"].ToString()));
-        }
+        SetChannels(json);
     }
 }
