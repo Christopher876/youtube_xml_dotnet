a2d987ed3d084fbfa97a269628c37da0bf3a0019
Christopher Williams <christopher876@outlook.com>
2020-03-03 19:31:38 -0500

    Refactor Channel - No longer has methods that it should not have - Only a class for data

--- Channel.cs
+++ Channel.cs
@@ -1,45 +1,4 @@
-using System.Xml.Linq;
-using System.Net.Http;
-using System;
-using HtmlAgilityPack;
-using System.IO;
-
 class YoutubeChannel{
     public string author;
     public string id;
-
-    public YoutubeChannel(){}
-
-    public YoutubeChannel(string author){
-        this.author = author;
-    }
-
-    public XDocument getChannelFeed(){
-        string url = Utils.channelBaseUrl+id;
-        string result = "";
-        using (var client = new HttpClient())
-        {
-            HttpResponseMessage response = client.GetAsync(url).Result;
-            response.EnsureSuccessStatusCode();
-            result = response.Content.ReadAsStringAsync().Result;
-        }
-        return XDocument.Parse(result);
-    }
-
-    public void getChannelId(){
-        string result;
-
-        //We get the channel html
-        using(var client = new HttpClient()){
-            HttpResponseMessage response = client.GetAsync("https://www.youtube.com/user/"+author).Result;
-            response.EnsureSuccessStatusCode();
-            result = response.Content.ReadAsStringAsync().Result;
-        }
-
-        //Process the html to get the channel id
-        var htmlDoc = new HtmlDocument();
-        htmlDoc.LoadHtml(result);
-        string channelId = htmlDoc.DocumentNode.SelectSingleNode("/html/head/link[5]").Attributes[1].Value;
-        this.id = channelId.Replace("https://www.youtube.com/channel/","");
-    }
 }
