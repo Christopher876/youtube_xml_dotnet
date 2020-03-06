281a4d1e313ba04c11ad71786fc74acdfbcf7737
Christopher Williams <christopher876@outlook.com>
2020-03-05 20:28:17 -0500

    Add Firebase class definition

---
+++ Firebase.cs
@@ -0,0 +1,15 @@
+using CorePush.Google;
+using Notifications;
+
+namespace FireBase
+{
+    class FireBase{
+        public FireBase(){
+
+        }
+
+        public void SendNotification(GoogleNotification notification){
+            
+        }
+    }
+}
