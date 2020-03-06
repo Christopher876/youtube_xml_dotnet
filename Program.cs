d07582ff8d27bcdca0a43dbb5f497a478bee87a6
Christopher Williams <christopher876@outlook.com>
2020-03-06 09:39:48 -0500

    Add CommandLineParser Options class - Implement the basics for an acutal command line program - Add force generation of database and option to add channels to an already created server instance

--- Program.cs
+++ Program.cs
@@ -1,21 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
+using CommandLine;
 
 namespace youtube_xml_dotnet
 {
     class Program
     {
+        public class Options{
+            [Option('l',"load-channels",Required=false,HelpText = "Load the \"channel-list.txt\" channels into the server")]
+            public bool LoadChannelList{get;set;}
+
+            [Option('f',"fore-intial",Required=false,HelpText = "Force server to regenerate the initial manager")]
+            public bool ForceInitialGeneration{get;set;}
+        }
+
         static void Main(string[] args)
         {
-            using(var db = new VideoContext()){
-                ChannelManager manager = new ChannelManager();
-                // manager.GenerateInitialManager(filename:"channels.txt",isFile:true);
+            //Ctrl+C Event - Stop and clean up things
+            Console.CancelKeyPress += delegate{
+                //TODO When Hangfire is added, clean up all hangfire background processes
+                Console.WriteLine("Exitting... No Hangfire Background Events to clean up");
+            };
+
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>( o => {
+                    ChannelManager manager = new ChannelManager();
+                    //Should we add any channels to our server?
+                    if(o.LoadChannelList){
+                        manager.AddChannels();
+                    }
+                    //Generate the initial files that are needed
+                    else if(!File.Exists(@"youtube-channels.json") || o.ForceInitialGeneration){
+                        manager.GenerateInitialManager();
+                    }
+                });
                 //manager.GenerateInitialManager();
-                manager.LoadChannelList();
-                manager.CheckForVideoUpdates();
-            }
+                //manager.LoadChannelList();
+                //manager.CheckForVideoUpdates();
             
         }
     }
